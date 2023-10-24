0b816e713dbc80fd335408149d9339e29dce5d3f
mgregoire254 <mgregoire1@me.com>
2023-10-24 17:26:50 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+# Python Web App Boilerplate 🚀
+
+---
+
+## 📘 Description
+
+With the ever-growing needs of web development, setting up the initial structure of a Flask project can be tedious. **Python Web App Boilerplate** simplifies this process, helping you get started faster by generating the basic file structure based on the name of your app.
+
+---
+
+## 🛣 Roadmap
+
+We're always looking to make Web App Boilerplate better. Here's a sneak peek at what's coming:
+
+- Generate contents for files to have a working "hello world" Flask app.
+
+Stay tuned for more updates!
+
+---
+
+## 💡 Usage
+
+1. **Clone or Download the Project**
+
+    ```bash
+    git clone [Repository Link]
+    ```
+
+    or
+
+    [Download the Code](#)
+
+2. **Run the Boilerplate Generator**
+
+    Navigate to the directory where you cloned or downloaded the project:
+
+    ```bash
+    cd path_to_directory
+    ```
+
+    Then, run the following command:
+
+    ```bash
+    python web_app_boilerplate.py
+    ```
+
+    Follow the prompts, and voilà! Your Flask project structure is ready.
+
+---
+
+## 🧑‍💻 Developer
+
+👤 **Michael Gregoire**
+
+---
+
+💡 _Have suggestions or issues? Please raise an issue on the GitHub repository or contact Michael directly._
+
---
+++ webapp_boilerplate.py
@@ -0,0 +1,61 @@
+"""A script that creates a boilerplate project for Flask web applications
+    Written by Michael Gregoire
+"""
+import os
+import shutil
+from shutil import Error
+
+# Take user preference input
+project_name = input("What is the name of your project? ")
+root_path = "./" + project_name
+
+'''Function to create directories'''
+def make_directory(path):
+    try:
+        os.mkdir(path)
+        print("Folder %s created!" % path)
+    except FileExistsError:
+        print("Folder %s already exists" % path)
+
+'''Function to create files'''
+def make_file(dir, name, cont, ext):
+    n = name + ext
+    try:
+        with open(n, "w") as file:
+            file.writelines([cont])
+            file.close()
+        print("File %s%s created successfully!" % (name, ext))
+    except FileExistsError:
+        print("File %s%s already exists" % (name, ext))
+
+    try:
+        if dir != "":
+            shutil.move(n, dir)
+            print("File %s moved to %s", (n, dir))
+    except Error as err:
+        print("File %s already exists in %s", (n, dir))           
+
+# Create Folder Structure
+make_directory(root_path)
+make_directory(root_path + "/templates")
+make_directory(root_path + "/static")
+make_directory(root_path + "/static/images")
+make_directory(root_path + "/tests")
+  
+# Data for files
+base = " "
+style = " "
+app = " "
+readme = "#" + project_name
+
+# Create Files
+make_file("", "app", app, ".py")
+make_file("./stuff/templates", "base", base, ".html")
+make_file("./stuff/static", "style", style, ".css")
+
+# Closing message
+print("Happy Coding!")
+
+# To Do
+# Create virtual environment
+# Install dependencies
